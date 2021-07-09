218a047b0783870fe46759671775270e5aa108cf
Leanne <leanne_porter@hotmail.com>
2021-07-09 13:21:30 +1000

    adding queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -79,3 +79,8 @@ OR departments.dept_name = 'Development';
 
 
 -- Question 8: In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) AS "Last name count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Last name count" DESC;
