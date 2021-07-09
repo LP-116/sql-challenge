f594872d1ee83873163b7b2ff6147dd97a896678
Leanne <leanne_porter@hotmail.com>
2021-07-09 12:28:38 +1000

    adding queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -20,9 +20,16 @@ WHERE hire_date LIKE '%1986';
 -- Question 3: List the manager of each department with the following information: 
 --             department number, department name, the manager's employee number, last name, first name.
 
-
-
-
+SELECT dept_manager.dept_no,
+	departments.dept_name,
+	dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM dept_manager
+INNER JOIN departments ON
+dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no = employees.emp_no;
 
 -- Question 4: List the department of each employee with the following information: 
 --             employee number, last name, first name, and department name.
