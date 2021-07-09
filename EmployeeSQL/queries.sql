99d7c5b3c1b4dd48b8dac8290031dbf5755f0ace
Leanne <leanne_porter@hotmail.com>
2021-07-09 13:01:48 +1000

    adding queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -42,7 +42,6 @@ dept_emp.emp_no = employees.emp_no
 INNER JOIN departments ON
 dept_emp.dept_no = departments.dept_no;
 
-
 -- Question 5: List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 SELECT first_name, last_name, sex
@@ -50,10 +49,18 @@ FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
 
-
 -- Question 6: List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-
+SELECT dept_emp.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM dept_emp
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no
+WHERE departments.dept_name = 'Sales';
 
 
 -- Question 7: List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
