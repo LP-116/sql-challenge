815f3312f4923499ed31638745e3a291d7b446b1
Leanne <leanne_porter@hotmail.com>
2021-07-12 10:50:55 +1000

    updating changes

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,44 +1,48 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
+DROP TABLE IF EXISTS Employees CASCADE;
 CREATE TABLE Employees (
     emp_no INT   NOT NULL,
-    emp_title_id VARCHAR   NOT NULL,
-    birth_date VARCHAR   NOT NULL,
+    emp_title_id VARCHAR(5)   NOT NULL,
+    birth_date VARCHAR(10)   NOT NULL,
     first_name VARCHAR   NOT NULL,
     last_name VARCHAR   NOT NULL,
-    sex VARCHAR   NOT NULL,
-    hire_date VARCHAR   NOT NULL,
+    sex VARCHAR(1)   NOT NULL,
+    hire_date VARCHAR(10)   NOT NULL,
     CONSTRAINT pk_Employees PRIMARY KEY (
         emp_no
      )
 );
 
+DROP TABLE IF EXISTS Departments CASCADE;
 CREATE TABLE Departments (
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
     dept_name VARCHAR   NOT NULL,
     CONSTRAINT pk_Departments PRIMARY KEY (
         dept_no
      )
 );
 
+DROP TABLE IF EXISTS Dept_emp CASCADE;
 CREATE TABLE Dept_emp (
     emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
     CONSTRAINT pk_Dept_emp PRIMARY KEY (
         emp_no,dept_no
      )
 );
 
+DROP TABLE IF EXISTS Dept_manager CASCADE;
 CREATE TABLE Dept_manager (
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
     emp_no INT   NOT NULL,
     CONSTRAINT pk_Dept_manager PRIMARY KEY (
         dept_no,emp_no
      )
 );
 
+DROP TABLE IF EXISTS Salaries CASCADE;
 CREATE TABLE Salaries (
     emp_no INT   NOT NULL,
     salary INT   NOT NULL,
@@ -47,8 +51,9 @@ CREATE TABLE Salaries (
      )
 );
 
+DROP TABLE IF EXISTS Titles CASCADE;
 CREATE TABLE Titles (
-    title_id VARCHAR   NOT NULL,
+    title_id VARCHAR(5)   NOT NULL,
     title VARCHAR   NOT NULL,
     CONSTRAINT pk_Titles PRIMARY KEY (
         title_id
@@ -72,4 +77,3 @@ REFERENCES Employees (emp_no);
 
 ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
 REFERENCES Employees (emp_no);
-
